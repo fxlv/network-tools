5f1c74b509f53b750ba21472874ee439a39e7e16
Kaspars Mickevics <kaspars@fx.lv>
2016-06-05 13:52:04 +0300

    Random functions added

--- src/common.c
+++ src/common.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "common.h"
 
+
+int randomint(int max_value){
+    // straight out of 'arc4random' manpage
+    return arc4random() % ((unsigned)max_value +1 );
+}
+    
+/*
+ * Convert integer to a string.
+ * Pass in a buffer to be used for the string and the length of the buffer.
+ * String will be truncated to fit in the buffer.
+ * */
+void int_to_str(int int_value, char* str_buf, int buf_len){
+    char stri[buf_len];
+    snprintf(stri, buf_len, "%d", int_value);
+    strncpy(str_buf, stri, buf_len);
+}
+
+// copy random octet (char*) into the provided buffer
+void random_octet(char* oct_buf){
+    int rand_int = randomint(254);
+    int_to_str(rand_int, oct_buf, 4); // max size always will be 4 (3 +1)
+}
 
 void die(char *msg)
 {
--- src/common.h
+++ src/common.h
@@ -2,5 +2,8 @@
 #define _common_h
 
 void die(char *msg);
+int randomint();
+void int_to_str(int int_value, char* str_buf, int buf_len);
+void random_octet(char* oct_buf);
 
 #endif
